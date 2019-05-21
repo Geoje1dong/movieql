ab5915744fea280147fb6f0b12fb7b9bb91fc969
Kim Jae Hyune <wogus4041@naver.com>
2019-05-21 15:17:02 +0900

    Delete Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,33 +1,33 @@
 let movies = [
     {
-        id:'0',
+        id:0,
         name: 'kimjaehyune',
-        score:'30'
+        score: 30
     },
     {
-        id:'1',
+        id:1,
         name: 'tomato',
-        score:'12'
+        score: 12
     },
     {
-        id:'2',
+        id: 2,
         name: 'smile',
-        score:'11'
+        score: 11
     }
 ];
 
 export const getMovies = () => movies;
 
 export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === String(id));
+    const filteredMovies = movies.filter(movie => movie.id === id);
     return filteredMovies[0];
 }
 
 export const deleteMovie = (id) => {
-    const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
     if(movies.length > cleanedMovies.length){
-        movie = cleanedMovies;
-        retrun
+        movies = cleanedMovies;
+        return true;
     }else{
         return false;
     }
@@ -35,7 +35,7 @@ export const deleteMovie = (id) => {
 
 export const addMovie = (name, score) => {
     const newMovie ={
-        id: `${movies.length + 1}`,
+        id: parseInt(`${movies.length + 1}`),
         name,
         score
     };
