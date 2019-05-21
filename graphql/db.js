3ca80e0df928f8d5263c853035e1f59e8f4e483d
Kim Jae Hyune <wogus4041@naver.com>
2019-05-21 14:33:29 +0900

    Creating first Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,25 +1,44 @@
-export const people = [
+let movies = [
     {
         id:'0',
         name: 'kimjaehyune',
-        age:'30',
-        gender: 'female;'
+        score:'30'
     },
     {
         id:'1',
         name: 'tomato',
-        age:'12',
-        gender: 'female;'
+        score:'12'
     },
     {
         id:'2',
         name: 'smile',
-        age:'11',
-        gender: 'female;'
+        score:'11'
     }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === String(id));
-    return filteredPeople[0];
+    const filteredMovies = movies.filter(movie => movie.id === String(id));
+    return filteredMovies[0];
+}
+
+export const deleteMovie = (id) => {
+    const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+    if(movies.length > cleanedMovies.length){
+        movie = cleanedMovies;
+        retrun
+    }else{
+        return false;
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie ={
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
 }
