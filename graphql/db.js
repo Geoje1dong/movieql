16115e371871d970f3ded864001e59d0f9c516dd
Kim Jae Hyune <wogus4041@naver.com>
2019-05-23 12:39:59 +0900

    node-fetch axios로 대체

--- graphql/db.js
+++ graphql/db.js
@@ -1,44 +1,46 @@
-let movies = [
-    {
-        id:0,
-        name: 'kimjaehyune',
-        score: 30
-    },
-    {
-        id:1,
-        name: 'tomato',
-        score: 12
-    },
-    {
-        id: 2,
-        name: 'smile',
-        score: 11
+import axios from "axios";
+const BASE_URL = "https://yts.am/api/v2/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
+
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies }
     }
-];
-
-export const getMovies = () => movies;
-
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
-}
-
-export const deleteMovie = (id) => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if(movies.length > cleanedMovies.length){
-        movies = cleanedMovies;
-        return true;
-    }else{
-        return false;
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating
     }
-}
+  });
+  console.log(movies);
+  return movies;
+};
 
-export const addMovie = (name, score) => {
-    const newMovie ={
-        id: parseInt(`${movies.length + 1}`),
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
-}
+export const getMovie = async id => {
+  const {
+    data: {
+      data: { movie }
+    }
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id
+    }
+  });
+  return movie;
+};
+
+export const getSuggestions = async id => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id
+    }
+  });
+  return movies;
+};
